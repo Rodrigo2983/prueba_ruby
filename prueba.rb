e352c8f43311eadd90b9506589e36aeae6657896
rodrigo sanchez <rodrigosancgezbecerra@gmail.com>
2019-01-10 15:23:09 -0300

    hoy un cero a la isquierda mis disculas

--- prueba.rb
+++ prueba.rb
@@ -8,7 +8,7 @@ end
 array = read_alum('nombre_de_alumnos.csv')
 
 def menu
-
+  puts "\n---------------------------------------------------------"
   puts "1)Nombre del alumno y promedio"
   puts "2)Inacistencias por alumno"
   puts "3)Alumnos aprobados"
@@ -20,4 +20,27 @@ menu()
 puts 'Ingrese una opcion'
 opcion = gets.chomp.to_i
 
-while 
+while opcion !=4
+
+	if opcion  == 1
+		array.each do |alu|
+			nombre = alu[0]
+			notas = alu.map{ |n| n.to_i}.sum
+			promedio = notas
+			puts "#{nombre}, y su promedio es de #{promedio}"
+		end
+
+	elsif opcion == 2
+
+	elsif opcion == 3
+		# haga la magia
+else
+	puts "ingrese un opcion valida"
+end
+
+menu()
+puts 'Ingrese una opcion'
+opcion = gets.chomp.to_i
+
+puts "adios"
+end
